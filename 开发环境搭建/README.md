aa50cc85dcd4c366a8a9114972c05aeee5b4623e
魏光远 <guangyuan.wei@rightc.cn>
2019-03-19 13:39:59 +0800

    node_graphql 提交

--- 开发环境搭建/README.md
+++ 开发环境搭建/README.md
@@ -2,3 +2,4 @@
 graphql-极简的spring-mvc配置 maven版本的 <br>
 javaWeb-简单的javaweb add到tomcat上可以直接跑 <br>
 springmvc_demo-引包版本的spring mvc 参考价值一般 <br>
+node_graphql-nodejs的graphql项目 非常轻的graphql服务 <br>
---
+++ 开发环境搭建/node_graphql/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import Schema from './src/schema'
+import graphqlHTTP from 'express-graphql'
+
+const app = express()
+const port = 3000
+
+app.all("*",function(req,res,next){
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin","*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers","content-type");
+    //跨域允许的请求方式 
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() == 'options')
+        res.send(200);  //让options尝试请求快速结束
+    else
+        next();
+});
+
+app.use('/', graphqlHTTP({
+    schema: Schema,
+    graphiql: true
+}));
+
+let server = app.listen(port, function () {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+});
